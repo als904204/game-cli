fe0d1baa12857f7ffca304efd66e8a75817f1e6f
Min <als904204@naver.com>
2025-06-03 16:43:33 +0900

    메인 애플리케이션

---
+++ src/main/application.py
@@ -0,0 +1,40 @@
+from src.user.user_manager import UserManager
+from src.view.cli import CLI
+
+def application():
+  user_manager = UserManager()
+  view = CLI()
+
+  logged_in_user = None
+
+  while True:
+    choice = view.display_main_menu()
+
+    if choice == "1":
+      username, password, password_confirm, = view.get_register_details()
+      success, message = user_manager.register(username, password, password_confirm)
+
+      if success:
+        view.display_message(message, "success")
+      else:
+        view.display_message(message, "error")
+
+    elif choice == "2":
+      if logged_in_user:
+        view.display_message(f"이미 '{logged_in_user.username}'님으로 로그인되어 있습니다.", "info")
+        continue # 메인 메뉴로 다시 돌아감
+
+      username, password = view.get_login_details()
+      success, message, obj = user_manager.login(username, password)
+      if success:
+        logged_in_user = obj
+        view.display_message(message, "success")
+      else:
+        view.display_message(message, "error")
+
+    elif choice == "3":
+      view.display_message("프로그램을 종료합니다...")
+      break
+
+if __name__ == "__main__":
+  application()
